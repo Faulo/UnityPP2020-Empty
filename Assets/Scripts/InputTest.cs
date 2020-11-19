b4f56ba7c82115e519a5adc64e666c3caa094c05
Daniel Schulz <faulolio@gmail.com>
2020-11-19 18:12:01 +0100

    Solve Testat02
    (cherry picked from commit 5b7b87c73656db83d07c0759dd8a8e9f92de2211)

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        var direction = Vector3.zero;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            direction += Vector3.up;
+        }
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            direction += Vector3.down;
+        }
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            direction += Vector3.left;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            direction += Vector3.right;
+        }
+        if (direction != Vector3.zero)
+        {
+            transform.position += direction.normalized * movementSpeed * Time.deltaTime;
+        }
+    }
+}
