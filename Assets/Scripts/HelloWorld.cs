1d2dafa2cd86fd24c65fb58a164a43dad114fd7b
Daniel Schulz <faulolio@gmail.com>
2020-11-19 18:11:20 +0100

    Solve Testat01
    (cherry picked from commit d6d021698ffa710738e2e7dd3aa0c8b48b29178f)

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Hallo Welt!");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
