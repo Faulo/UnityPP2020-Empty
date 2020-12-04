0589b746a80214a0846e2d129652983e09ee0c22
Daniel Schulz <faulolio@gmail.com>
2020-12-04 18:57:07 +0100

    Solve Testat 04

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -8,12 +8,16 @@ public class AvatarController : MonoBehaviour
     public float jumpSpeed = 5;
     public InputAction movementAction = default;
     public InputAction jumpAction = default;
+    public Color avatarColor = default;
 
     private Rigidbody2D attachedRigidbody;
+    private Renderer attachedRenderer;
 
     private void Start()
     {
         attachedRigidbody = GetComponent<Rigidbody2D>();
+        attachedRenderer = GetComponentInChildren<Renderer>();
+        SetRendererColor(avatarColor);
     }
 
     private void OnEnable()
@@ -44,10 +48,19 @@ public class AvatarController : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded = true;
+        if (collision.gameObject.TryGetComponent<ColoredPlatform>(out var platform))
+        {
+            SetRendererColor(platform.platformColor);
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
+    }
+
+    private void SetRendererColor(Color color)
+    {
+        attachedRenderer.material.SetColor("_BaseColor", color);
     }
 }
---
+++ Assets/Scripts/ColoredPlatform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ColoredPlatform : MonoBehaviour
+{
+    public Color platformColor;
+    public Renderer attachedRenderer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (attachedRenderer)
+        {
+            attachedRenderer.material.SetColor("_BaseColor", platformColor);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
