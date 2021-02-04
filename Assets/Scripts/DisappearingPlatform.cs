9d17765fea3f155eb1b04dc3212c8cda0ddbf933
Daniel Schulz <faulolio@gmail.com>
2021-02-04 15:25:21 +0100

    Add DisappearingPlatform stub

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    public Collider2D attachedCollider;
+    public Renderer attachedRenderer;
+
+    private void Start()
+    {
+        StartCoroutine(DisappearRoutine());
+    }
+
+    private IEnumerator DisappearRoutine()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log(this);
+    }
+}
