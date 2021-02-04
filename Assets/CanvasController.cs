acb54d19dab1a8e4287b9d96bc19c9bd264d82f6
Daniel Schulz <faulolio@gmail.com>
2021-02-04 18:06:14 +0100

    Implement disappearing platform, hello world canvas

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        text.text = "Hallo Welt!";
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
--- Assets/Scripts/DisappearingPlatform.cs
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -6,6 +6,15 @@ public class DisappearingPlatform : MonoBehaviour
     public Collider2D attachedCollider;
     public Renderer attachedRenderer;
 
+    [SerializeField, Range(0, 5)]
+    private float visibleDuration = 1;
+    [SerializeField, Range(0, 5)]
+    private float disappearingDuration = 1;
+    [SerializeField, Range(0, 5)]
+    private float goneDuration = 1;
+    [SerializeField, Range(0, 5)]
+    private float appearingDuration = 1;
+
     private void Start()
     {
         StartCoroutine(DisappearRoutine());
@@ -13,7 +22,39 @@ public class DisappearingPlatform : MonoBehaviour
 
     private IEnumerator DisappearRoutine()
     {
-        yield return new WaitForSeconds(1);
-        Debug.Log(this);
+        yield return new WaitForEndOfFrame();
+        Color color = attachedRenderer.material.color;
+        while (true)
+        {
+            // Collider ist aktiv, Sprite ist sichtbar
+            yield return new WaitForSeconds(visibleDuration);
+
+            // Collider ist aktiv, Sprite verschwindet
+            for (float timer = 0; timer < disappearingDuration; timer += Time.deltaTime)
+            {
+                color.a = 1 - (timer / disappearingDuration);
+                attachedRenderer.material.color = color;
+                yield return null;
+            }
+            color.a = 0;
+            attachedRenderer.material.color = color;
+
+            // Collider ist inaktiv, Sprite ist unsichtbar
+            attachedCollider.enabled = false;
+
+
+            yield return new WaitForSeconds(goneDuration);
+
+            // Collider ist aktiv, Sprite erscheint
+            attachedCollider.enabled = true;
+            for (float timer = 0; timer < appearingDuration; timer += Time.deltaTime)
+            {
+                color.a = timer / appearingDuration;
+                attachedRenderer.material.color = color;
+                yield return null;
+            }
+            color.a = 1;
+            attachedRenderer.material.color = color;
+        }
     }
 }
